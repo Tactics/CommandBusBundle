157d35f3b7d40d5aec666a4bd866b9befce2b3fd
Bart Ruysseveldt <bart@tactics.be>
2014-12-08 15:50:59 +0100

    add compiler pass to container

--- TacticsCommandBusBundle.php
+++ TacticsCommandBusBundle.php
@@ -2,8 +2,24 @@
 
 namespace Tactics\CommandBusBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+use Tactics\CommandBusBundle\DependencyInjection\CommandHandlerPass;
+
+/**
+ * Class TacticsCommandBusBundle
+ * @package Tactics\CommandBusBundle
+ */
 class TacticsCommandBusBundle extends Bundle
 {
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new CommandHandlerPass());
+    }
 }
