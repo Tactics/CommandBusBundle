731efa45600c826cb7dc1c4283ba716327e90efe
JorisH <joris@tactics.be>
2024-06-26 14:36:05 +0200

    autotag command handlers

--- DependencyInjection/TacticsCommandBusExtension.php
+++ DependencyInjection/TacticsCommandBusExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Tactics\CommandBusBundle\CommandHandler\CommandHandler;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -24,5 +25,8 @@ class TacticsCommandBusExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $container->registerForAutoconfiguration(CommandHandler::class)
+            ->addTag('command_handler');
     }
 }
